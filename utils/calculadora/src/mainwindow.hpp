820001888b464e3dd2397de230bad6e013f94408
Rgeditv1 <rgeditv1@protonmail.com>
2026-03-21 17:37:35 -0400

    logica añadida

--- utils/calculadora/src/mainwindow.hpp
+++ utils/calculadora/src/mainwindow.hpp
@@ -1,14 +1,25 @@
 #pragma once
 #include <QMainWindow>
+#include <QString>
+#include <cstdint>
 #include "./gui/ui_mainwindow.hpp"
+
 
 class MainWindow : public QMainWindow
 {
     Q_OBJECT
 
+private:
+    Ui::Calculadora* ui;
+    double valor_actual = 0;
+    double valor_previo = 0;
+    char operacion_pendiente = 0;
+    bool esperando_segundo = false;
+
 public:
     explicit MainWindow(QWidget *parent = nullptr);
+    ~MainWindow();
 
-private:
-    Ui::Calculadora ui;
+    void onOperacion(char op);
+    void onIgual();
 };
